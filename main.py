6bd6508c90092df16ec9472b69a73b946b37f99d
dalida <dalida@HOME-PC.localdomain>
2026-07-01 18:48:35 +0500

    base commands fixed

---
+++ main.py
@@ -0,0 +1,21 @@
+#  git - система контроля версией
+
+# github/ gitlab/ bitbucket - хранилище\сайт хранение (версией) кода
+
+
+# repository/репозиторий - папка, в которой гит отслеживает изменения
+
+# git init - создание репозитория
+
+# git status - проверка статуса отслеживания
+
+# git add <file_name> or . - добавление файла в список отслеживания(в staging area)
+
+# git commit -m 'some message' - фиксация\создание версии кода с сообщением
+
+#  git remote add <variable_name>(usually origin) <link to repository> - 
+# свяска локального репозитория с удаленным и сохранение ссылки в переменную
+# git remote -v - проверить(неважно)
+# git push origin <branch_name> - отправка версии кода в удаленный репозиторий
+
+
